8ad948cda09ba1067d420f0427c20ea8b3a4a4ef
ghazanfarali1994 <ghazanfar006@gmail.com>
2023-05-28 14:08:36 +0500

    adding tailwind css and debugging babelrc error 34

---
+++ codeswear/pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>CodesWear.com - Wear the code</title>
+        <meta name="description" content="CodesWear.com - Wear the code" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      Hey This is Codeswear
+      <div className="mx-4">This is me</div>
+      <div className="mx-4 bg-red-500 ">This is me</div>
+      
+    </>
+  )
+}
